7fe0487134bee6f0b19d6a2e296c20d920173f98
Lisa Sampson <lisasampson0918@gmail.com>
2018-08-07 20:57:07 -0400

    Set up Detail View

--- ExerciseApp/View Controllers/DetailViewController.swift
+++ ExerciseApp/View Controllers/DetailViewController.swift
@@ -12,28 +12,40 @@ class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        updateViews()
     }
     
     @IBAction func saveExerciseButtonTapped(_ sender: Any) {
+        guard let exerciseText = exerciseTextField.text,
+            let exerciseDetail = exerciseDetailTextView.text else { return }
         
+        if let exercise = exercise {
+            exerciseController?.updateExercise(newExercise: exercise, exercise: exerciseText, exerciseDetail: exerciseDetail)
+        } else {
+            exerciseController?.create(exercise: exerciseText, exerciseDetail: exerciseDetail)
+        }
+        navigationController?.popViewController(animated: true)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        guard isViewLoaded else { return }
+        
+        if let exercise = exercise {
+            navigationController?.title = exercise.exercise
+            exerciseTextField.text = exercise.exercise
+            exerciseDetailTextView.text = exercise.exerciseDetail
+        } else {
+            navigationController?.title = "Add New Exercise Routine"
+        }
     }
-    */
+   
+    var exerciseController: ExerciseController?
+    
+    var exercise: Exercise? {
+        didSet {
+            updateViews()
+        }
+    }
 
     @IBOutlet weak var exerciseTextField: UITextField!
     @IBOutlet weak var exerciseDetailTextView: UITextView!
