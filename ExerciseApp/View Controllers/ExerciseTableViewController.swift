f78c9b318915fb9066eaa263adbe2f5f2a345f55
Lisa Sampson <lisasampson0918@gmail.com>
2018-08-07 21:18:24 -0400

    Create final connections for an Exercise checklist app

--- ExerciseApp/View Controllers/ExerciseTableViewController.swift
+++ ExerciseApp/View Controllers/ExerciseTableViewController.swift
@@ -8,88 +8,89 @@
 
 import UIKit
 
-class ExerciseTableViewController: UITableViewController {
+class ExerciseTableViewController: UITableViewController, ExerciseTableViewCellDelegate {
+    
+    func toggleHasBeenDoneButton(for cell: ExerciseTableViewCell) {
+        guard let index = tableView.indexPath(for: cell) else { return }
+        let exercise = exerciseFor(indexPath: index)
+        
+        exerciseController.updateHasBeenDone(newExercise: exercise)
+        tableView.reloadData()
+    }
+    
+    func exerciseFor(indexPath: IndexPath) -> Exercise {
+        if indexPath.section == 0 {
+            return exerciseController.finishedExercises[indexPath.row]
+        } else {
+            return exerciseController.toDoExercises[indexPath.row]
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        exerciseController.loadFromPersistence()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        if section == 0 {
+            return exerciseController.finishedExercises.count
+        } else {
+            return exerciseController.toDoExercises.count
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Done for Today"
+        } else if section == 1 {
+            return "To Do Today"
+        }
+        return "Error"
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ExerciseCell", for: indexPath) as? ExerciseTableViewCell else { return UITableViewCell() }
 
-        // Configure the cell...
+        let exercise = exerciseFor(indexPath: indexPath)
+        cell.delegate = self
+        cell.exercise = exercise
 
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            let exercise = exerciseFor(indexPath: indexPath)
+            exerciseController.delete(newExercise: exercise)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "UpdateExerciseSegue" {
+            guard let indexPath = tableView.indexPathForSelectedRow, let detailVC = segue.destination as? DetailViewController else { return }
+            let exercise = exerciseFor(indexPath: indexPath)
+            detailVC.exerciseController = exerciseController
+            detailVC.exercise = exercise
+        } else if segue.identifier == "AddExerciseSegue" {
+            guard let detailVC = segue.destination as? DetailViewController else { return }
+            detailVC.exerciseController = exerciseController
+        }
     }
-    */
 
+    let exerciseController = ExerciseController()
+    
 }
